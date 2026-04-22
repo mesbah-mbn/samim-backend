423c330ba060d4a1dece7040b97e33a6b9f697d7
Mesbah Naraiwall <75528703+mesbah-mbn@users.noreply.github.com>
2026-04-22 07:32:09 +0200

    Add permission classes to Product and Lead views

--- api/views.py
+++ api/views.py
@@ -1,17 +1,25 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from .models import Product, Lead
 from .serializers import ProductSerializer, LeadSerializer
+
 
 # GET products (for slider)
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
 
     def get_serializer_context(self):
         return {'request': self.request}
 
 
 # POST lead (from form)
+@method_decorator(csrf_exempt, name='dispatch')
 class LeadCreateView(generics.CreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+    permission_classes = [AllowAny]
